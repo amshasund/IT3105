96d52e634e8a32b3f1f89e2cdc8ab5e7f15400e7
Anne Marie Skaar Hasund <amshasund@gmail.com>
2022-03-03 10:27:47 +0100

    Init structure

---
+++ project_2/parameters.py
@@ -0,0 +1,31 @@
+"""
+• The size (k) of the k x k Hex board, where 3 ≤ k ≤ 10.
+• Standard MCTS parameters, such as the number of episodes, number of search games per actual move, etc.
+• In the ANET, the learning rate, the number of hidden layers and neurons per layer, along with any of the
+following activation functions for hidden nodes: linear, sigmoid, tanh, RELU.
+• The optimizer in the ANET, with (at least) the following options all available: Adagrad, Stochastic Gradient
+Descent (SGD), RMSProp, and Adam.
+• The number (M) of ANETs to be cached in preparation for a TOPP. These should be cached, starting with an
+untrained net prior to episode 1, at a fixed interval throughout the training episodes.
+• The number of games, G, to be played between any two ANET-based agents that meet during the round-robin
+play of the TOPP.
+"""
+
+# TODO: FIX THESE
+
+hex_board_size = 3      # 3 <= k <= 10
+
+# mcts parameters
+episodes = 1
+search_games_per_move = 1
+# etc
+
+# actor net
+learning_rate = 0.1
+hidden_layers = [1, 5, 2, 4]
+activation_function = ["linear", "sigmoid", "tanh", "RELU"]
+optimizer = "sgd"  # adagrad, stochastic gradient descent, rmsprop or adam
+num_cached = 3
+
+# round-robin play
+num_games = 5
---
+++ project_2/rlsystem.py
@@ -0,0 +1,11 @@
+# From pseudocode
+
+from mimetypes import init
+
+
+class RLSystem:
+    def __init__(self) -> None:
+        pass
+
+    def algorithm():
+        i_s =

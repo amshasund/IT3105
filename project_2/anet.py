06f82ffc706ad72eff782b01448daface78e2b02
amshasund <amshasund@gmail.com>
2022-03-08 16:02:50 +0100

    Tips from studass

--- project_2/anet.py
+++ project_2/anet.py
@@ -9,35 +9,43 @@ from parameters import (
 )
 
 
-
 class ANet:
     def __init__(self):
-        self.num_input_nodes = hex_board_size**2
+        self.num_input_nodes = hex_board_size**2 + \
+            1  # one extra input to know which player
         self.model = None
 
-    def build_model(self,loss=tf.keras.losses.MeanSquaredError()):
+    # loss: cross-entropy, output for each move
+
+    # This comparison of output and target probablity distributions
+    # normally calls for a cross-entropy loss/error function; and to
+    # produce a probability distribution in the output vector, the
+    # softmax activation function is recommended.
+
+    def build_model(self, loss=tf.keras.losses.MeanSquaredError()):
         # Create Neural Net
         model = tf.keras.models.Sequential()
 
+        # TODO: No need for this
         # Initialize weights and biases
-        initializer = tf.keras.initializer.TruncatedNormal(mean=0., stddev=1.) # TODO
+        initializer = tf.keras.initializer.TruncatedNormal(
+            mean=0., stddev=1.)  # TODO
 
         # Add input layer
-        model.add(tf.keras.layers.Input(shape=(self.num_input_nodes,))) # TODO
+        model.add(tf.keras.layers.Input(shape=(self.num_input_nodes,)))  # TODO
 
         # Add hidden layers
         for i in range(len(hidden_layers)):
             model.add(tf.keras.layers.Dense(
-                hidden_layers[i], 
-                activation=activation_function[i], 
-                kernel_initializer = initializer,       # TODO
-                use_bias = True,                        # TODO
-                bias_initializer = initializer))        # TODO
-            
-        
+                hidden_layers[i],
+                activation=activation_function[i],
+                kernel_initializer=initializer,       # TODO
+                use_bias=True,                        # TODO
+                bias_initializer=initializer))        # TODO
+
         # Add output layer
-        model.add(tf.keras.layers.Dense(1, activation='sigmoid')) # TODO
-        
+        # TODO: output size same as amount of possible moves (hex board size)
+        model.add(tf.keras.layers.Dense(1, activation='sigmoid'))  # TODO
 
         # Using stochastic gradient descent when compiling
         # Optimizer is a string, could be a problem that it is not a tf function
--- project_2/todos.md
+++ project_2/todos.md
@@ -1,5 +1,7 @@
 ## Questions to studass
-- [ ] Can we use same initializer for weights and biases in neural net
-- [ ] What values should we use in the initializer
-- [ ] Should we initialize weights and biases in input/output layer as well?
-- [ ] Should we use MeanSquaredError as loss function?
+- [x] Can we use same initializer for weights and biases in neural net
+- [x] What values should we use in the initializer
+- [x] Should we initialize weights and biases in input/output layer as well?
+- [x] Should we use MeanSquaredError as loss function? -> cross-entropy
+
+- State: add one for which player makes the move

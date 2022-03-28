fix smal error such that rl runs until backprop.
Co-authored-by: amshasund <amshasund@users.noreply.github.com>

project_2/anet.py
@@ -1,11 +1,13 @@
 import numpy as np
 import tensorflow as tf
+import random
 
 from parameters import (
     hex_board_size,
     hidden_layers,
     activation_function,
-    optimizer,
+    optimizer, 
+    starting_player
 )
 
 
@@ -15,30 +17,24 @@ class ANet:
             1  # one extra input to know which player
         self.model = None
 
-    # loss: cross-entropy, output for each move
-
-    # This comparison of output and target probablity distributions
-    # normally calls for a cross-entropy loss/error function; and to
-    # produce a probability distribution in the output vector, the
-    # softmax activation function is recommended.
-    
     def build_model(self, loss=tf.keras.losses.BinaryCrossentropy):
         # Create Neural Net
         model = tf.keras.models.Sequential()
 
         # Add input layer
-        model.add(tf.keras.layers.Input(shape=(self.num_input_nodes,))) 
+        model.add(tf.keras.layers.Input(shape=(self.num_input_nodes,)))
 
         # Add hidden layers
         for i in range(len(hidden_layers)):
             model.add(tf.keras.layers.Dense(
                 hidden_layers[i],
-                activation=activation_function[i])) 
+                activation=activation_function[i]))
 
         # Add output layer
-        # TODO: should we remove the already placed pieces from this? 
+        # TODO: should we remove the already placed pieces from this?
         # should not be able to move pieces to a place where another already exists.
-        model.add(tf.keras.layers.Dense(hex_board_size**2, activation='softmax')) 
+        model.add(tf.keras.layers.Dense(
+            hex_board_size**2, activation='softmax'))
 
         # Using stochastic gradient descent when compiling
         # Optimizer is a string, could be a problem that it is not a tf function
@@ -53,14 +49,26 @@ class ANet:
         # Output: probability distribution over all legal moves
         state = self.reshape_state(state)
         target = reward + discount_factor_critic * self.get_value(new_state)
-        self.nn_model.fit(state, target, verbose=0)
-    
+        self.model.fit(state, target, verbose=0)
+
     def choose_move(self, state, legal_moves):
         # Explorative for rollout (behaviour (default) policy)
         # More exploitative for actual moves (target policy)
-        state = self.reshape_state(state)
-        value = self.nn_model(state)
-        return value
+        #state = self.reshape_state(state)
+        
+        #value = self.model(state)
+        
+        if state[1]:
+            player = (2 if state[1].get_player() == 1 else 1)
+        else:
+            player = starting_player
+        # For test
+        return [player, random.choice(legal_moves)]
 
-    def reshape_state(state):
-        pass
+    def reshape_state(self, state):
+        """Reshapes the state from [[GameBoard], [Player]] to [gamebord1D, player]"""
+        reshaped_state = []
+        reshaped_state.append(state[0].flatten())
+        reshaped_state.append(state[1])
+        print("reshaped state", reshaped_state)
+        return reshaped_state
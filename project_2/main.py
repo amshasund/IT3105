41553121fc4b084dc4632e2f7e03081a4cfe335a
ikolderu <ikolderu@cisco.com>
2022-04-06 14:59:59 +0200

    little possible 4x4 correct

--- project_2/main.py
+++ project_2/main.py
@@ -26,7 +26,7 @@ def main():
     tournament.play_tournament()
     
     # Random
-    model = tf.keras.models.load_model("best_models/little_possible_model_3x3_500.h5")
+    model = tf.keras.models.load_model("best_models/little_possible_model_4x4_500.h5")
     play_model_against_random(model)
 
 
--- project_2/rlsystem.py
+++ project_2/rlsystem.py
@@ -80,7 +80,7 @@ class RLSystem:
                 self.anet.save_model(actual_game)
             
     def write_rbuf_to_file(self, rbuf):
-        output_file = open('little_possible_3x3.txt', 'w')
+        output_file = open('little_possible_4x4.txt', 'w')
 
         for element in rbuf:
             state = list(element[0])
